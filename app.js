eb046548391da7cd066e048111c38d4c2c2883c1
davidwang <davidwang2006@qq.com>
2016-12-23 17:14:12 +0800

    改为上传下载目录是可配置的-root dir

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
 /**
  * cell phone file server
  * author: D.W.
+ * 
+ * usage:
+ * node app.js -root /somedir
+ * 
+ * @modified @2016-12-23 16:59:45 改为映射目录为可从参数传递的 
  * */
 
 'use strict';
 var express = require('express'),
 	connect = require('connect'),
 	fs = require('fs'),
+	path = require('path'),
     os = require('os');
+
+const DEFAULT_DIR = {
+	name: '/share',
+	path: 'D:/share'
+};
+
+var dir = DEFAULT_DIR;
 
 var app = express();
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+
+var index = process.argv.indexOf('-root');
+if(~index){
+	dir.path = process.argv[index+1];
+	dir.name = '/'+path.basename(dir.path);
+}
+
+usage();
+console.log('Now you try to serve @dir ',dir);
+
 var dirs = [];
-var upload_store_dir = 'D:/share';
-// ele -> {name: 'xxx',path:'/a/b'}
-dirs.push({
-	name: '/share',
-	path: 'D:/share'
-});
+var upload_store_dir = dir.path;
+dirs.push(dir);
 
 app.set('views',__dirname+'/views');
 app.set('view engine','jade');
 app.set('port',6677);
 
 app.use(connect.favicon());
-app.use(connect.multipart({defer:true,limit:'10mb'}));
+app.use(connect.multipart({defer:true,limit:'100mb'}));
 app.use(connect.errorHandler('dev'));
 
 app.get('/',function(req,res,next){
@@ -32,6 +51,7 @@ app.get('/',function(req,res,next){
 });
 app.post('/',function(req,res,next){
 	req.form.on('fileBegin',function(file,part){
+		//此处直接改变part.path 比接收完重命名 高效不少
 		part.path = upload_store_dir+'/'+part.name;
 	});
     req.form.on('file',function(file,part){
@@ -62,6 +82,9 @@ app.post('/',function(req,res,next){
 	*/
 });
 
+/**
+ * @deprecated use part.path = 'xxx' direct change
+ */
 function storeFile(file){
     if(os.platform() == 'linux')
         fs.rename(file.path,upload_store_dir+'/'+file.name,function(err){
@@ -87,3 +110,8 @@ app.listen(app.get('port'),function(err){
 	if(err) return console.log(err);
 	console.log('server start listening at port: '+app.get('port'));
 });
+
+
+function usage(){
+	console.log('USAGE:: node app.js -root somedirectory');
+}
